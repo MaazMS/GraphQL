5dee1d3664d2d5a72527ef897b8643a85abc2884
MaazMS <maazshaikh7020@gmail.com>
2021-03-14 10:57:34 +0530

    project schema

--- django_graphql_projects/projects/schema.py
+++ django_graphql_projects/projects/schema.py
@@ -21,7 +21,7 @@ class ProjectType(DjangoObjectType):
 class Query(ObjectType):
     developer = graphene.Field(DeveloperType, id=graphene.Int())
     project = graphene.Field(ProjectType, id=graphene.Int())
-    developers = graphene.List(DeveloperType)
+    developers = graphene.List(DeveloperType) # List use a type modifier, which indicates that this field will return a list.
     projects = graphene.List(ProjectType)
 
     def resolve_developer(self, info, **kwargs):
@@ -56,14 +56,15 @@ class DeveloperInput(graphene.InputObjectType):
 class ProjectInput(graphene.InputObjectType):
     id = graphene.ID()
     title = graphene.String()
-    developer = graphene.List(DeveloperInput)
+    developers = graphene.List(DeveloperInput)
     year = graphene.Int()
 
 # create mutation for developers
-class CreateDevelooper(graphene.Mutation):
+class CreateDeveloper(graphene.Mutation):
     class Arguments:
         input = DeveloperInput(required=True)
 
+    # developer and ok are the output fields of the Mutation when it is resolved.
     ok = graphene.Boolean()
     developer = graphene.Field(DeveloperType)
 
@@ -72,7 +73,7 @@ class CreateDevelooper(graphene.Mutation):
         ok = True
         developer_instance = Developer(name=input.name)
         developer_instance.save()
-        return CreateDevelooper(ok=ok, developer=developer_instance)
+        return CreateDeveloper(ok=ok, developer=developer_instance)
 
 
 # update mutation for developer
@@ -81,11 +82,12 @@ class UpdateDeveloper(graphene.Mutation):
         id = graphene.Int(required=True)
         input = DeveloperInput(required=True)
 
+    # developer and ok are the output fields of the Mutation when it is resolved.
     ok = graphene.Boolean()
     developer = graphene.Field(DeveloperType)
 
     @staticmethod
-    def mutate(root, info, input=None):
+    def mutate(root, info, id, input=None):
         ok = False
         developer_instance = Developer.objects.get(pk=id)
 
@@ -111,7 +113,7 @@ class CreateProject(graphene.Mutation):
         developers = []
 
         for developer_input in input.developers:
-            developer = Developer.objects.ger(pk=developer_input.id)
+            developer = Developer.objects.get(pk=developer_input.id)
             if developer is None:
                 return CreateProject(ok=False, Project=None)
             developers.append(developer)
@@ -132,7 +134,7 @@ class UpdateProject(graphene.Mutation):
     project = graphene.Field(ProjectType)
 
     @staticmethod
-    def mutate(root, info, input=None):
+    def mutate(root, info, id, input=None):
         ok = False
         project_instance = Project.objects.get(pk=id)
         if project_instance:
@@ -141,21 +143,56 @@ class UpdateProject(graphene.Mutation):
             for developer_input in input.developers:
                 developer = Developer.objects.get(pk=developer_input.id)
                 if developer is None:
-                    return UpdateDeveloper(ok=False, project=project_instance)
+                    return UpdateDeveloper(ok=False, project=None)
                 developers.append(developer)
             project_instance.title = input.title
-            project_instance.year = input.year.save()
+            project_instance.year = input.year
+            project_instance.save()
             project_instance.developers.set(developers)
             return UpdateProject(ok=ok, project=project_instance)
         return UpdateProject(ok=ok, project=None)
 
+# delete mutation for developer
+class DeleteDeveloper(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    ok = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        developer_instance = Developer.objects.get(pk=id)
+        if developer_instance:
+            ok = True
+            developer_instance.delete()
+            return DeleteDeveloper(ok=ok)
+
+# delete mutation for project
+class DeleteProject(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    ok = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        project_instance = Project.objects.get(pk=id)
+        if project_instance:
+            ok = True
+            project_instance.delete()
+            return DeleteProject(ok=ok)
+
 
 # create the Mutation type
 class Mutation(graphene.ObjectType):
-    create_developer = CreateDevelooper.Field()
+    create_developer = CreateDeveloper.Field()
     update_developer = UpdateDeveloper.Field()
     create_project = CreateProject.Field()
     update_project = UpdateProject.Field()
+    delete_developer = DeleteDeveloper.Field()
+    delete_project = DeleteProject.Field()
 
 
 # Making the Schema to map the queries and mutations to our application's API.
